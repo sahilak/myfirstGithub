b0050b63f243ecaf5611333685fe69b35093b877
sahilak <sahila.karra@gmail.com>
2019-07-07 09:14:51 -0400

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,10 @@
+public class main
+{
+public static void main(string{} args)
+{
+/**************
+write a program that ads Ato b and store the result in c
+where A=3,b=4,C=5
+***************/
+}
+}
