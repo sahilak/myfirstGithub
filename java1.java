d84a9cfea9e61ac5d783d3b4dc9c93030b51b536
sahilak <sahila.karra@gmail.com>
2019-07-07 09:54:11 -0400

    Update java1.java

--- java1.java
+++ java1.java
@@ -6,5 +6,9 @@ public static void main(string{} args)
 write a program that ads Ato b and store the result in c
 where A=3,b=4,C=5
 ***************/
+  int A=3;
+  int b=4;
+  int c= A+B;
+  i
 }
 }
